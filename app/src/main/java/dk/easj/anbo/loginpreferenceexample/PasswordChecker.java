cfcbe04b9536de006e1f3c538bd136b58eb7109a
Anders Børjesson <anbo@zealand.dk>
2020-03-02 09:42:19 +0100

    Gradle update

--- app/src/main/java/dk/easj/anbo/loginpreferenceexample/PasswordChecker.java
+++ app/src/main/java/dk/easj/anbo/loginpreferenceexample/PasswordChecker.java
@@ -4,6 +4,6 @@ class PasswordChecker {
 
     // TODO: This method needs to do some proper password checking
     public static boolean Check(String username, String password) {
-        return password.startsWith("a");
+        return password.startsWith("s");
     }
 }
